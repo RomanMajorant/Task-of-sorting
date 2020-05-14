ea79d54211bc34c7be727fc4056230414a60bb30
Rick1Sanchez <makotovchuk@bk.ru>
2020-05-14 23:49:40 +0300

    done
    done

---
+++ VsualSorfWF/VsualSorfWF/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace VsualSorfWF
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maxLim = new System.Windows.Forms.NumericUpDown();
+            this.minLim = new System.Windows.Forms.NumericUpDown();
+            this.countElements = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxLim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minLim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countElements)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.maxLim);
+            this.groupBox1.Controls.Add(this.minLim);
+            this.groupBox1.Controls.Add(this.countElements);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 408);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(785, 61);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки";
+            // 
+            // maxLim
+            // 
+            this.maxLim.Location = new System.Drawing.Point(233, 35);
+            this.maxLim.Name = "maxLim";
+            this.maxLim.Size = new System.Drawing.Size(122, 20);
+            this.maxLim.TabIndex = 9;
+            this.maxLim.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // minLim
+            // 
+            this.minLim.Location = new System.Drawing.Point(111, 35);
+            this.minLim.Name = "minLim";
+            this.minLim.Size = new System.Drawing.Size(116, 20);
+            this.minLim.TabIndex = 8;
+            // 
+            // countElements
+            // 
+            this.countElements.Location = new System.Drawing.Point(6, 35);
+            this.countElements.Name = "countElements";
+            this.countElements.Size = new System.Drawing.Size(99, 20);
+            this.countElements.TabIndex = 7;
+            this.countElements.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(680, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Максимальный предел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Минимальный предел";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кол-во элементов ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(785, 390);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Отображение массива";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(773, 365);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 481);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(506, 342);
+            this.Name = "Form1";
+            this.Text = "Визуализация сортировки";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.maxLim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minLim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countElements)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.NumericUpDown maxLim;
+		private System.Windows.Forms.NumericUpDown minLim;
+		private System.Windows.Forms.NumericUpDown countElements;
+	}
+}
+
---
+++ VsualSorfWF/VsualSorfWF/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VsualSorfWF
+{
+	public partial class Form1 : Form
+	{
+		public int[] array;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+        public void mergeSort(int[] arr,
+                           int l, int r)
+        {
+            if (l < r)
+            {
+
+        
+                int m = l + (r - l) / 2;
+                mergeSort(arr, l, m);
+                mergeSort(arr, m + 1, r);
+                merge(arr, l, m, r);
+            }
+            drawSort(array);
+            drawMarking();
+            Thread.Sleep(60);
+        }
+
+        static void merge(int[] arr, int l,
+                           int m, int r)
+        {
+            int i, j, k;
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            /* create temp arrays */
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            /* Copy data to temp arrays 
+            L[] and R[] */
+            for (i = 0; i < n1; i++)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; j++)
+                R[j] = arr[m + 1 + j];
+
+            /* Merge the temp arrays back  
+            into arr[l..r]*/
+            i = 0;
+            j = 0;
+            k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            /* Copy the remaining elements of 
+            L[], if there are any */
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            /* Copy the remaining elements of 
+            R[], if there are any */
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+
+        }
+        /*public Int32[] Merge_sort(Int32[] massive)
+        {
+            if (massive.Length == 1)
+                return massive;
+            Int32 mid_point = massive.Length / 2;
+            return Merge(Merge_sort(massive.Take(mid_point).ToArray()), Merge_sort(massive.Skip(mid_point).ToArray()));
+  
+        }
+
+        public Int32[] Merge(Int32[] mass1, Int32[] mass2)
+        {
+            Int32 a = 0, b = 0;
+            Int32[] merged = new int[mass1.Length + mass2.Length];
+            for (Int32 i = 0; i < mass1.Length + mass2.Length; i++)
+            {
+                if (b < mass2.Length && a < mass1.Length)
+                    if (mass1[a] > mass2[b])
+                        merged[i] = mass2[b++];
+                    else //if int go for
+                        merged[i] = mass1[a++];
+                else
+                    if (b < mass2.Length)
+                    merged[i] = mass2[b++];
+                else
+                    merged[i] = mass1[a++];
+
+                
+            }
+            drawSort(array);
+            drawMarking();
+            Thread.Sleep(100);
+            return merged;
+        }*/
+
+        private void button1_Click( object sender, EventArgs e )
+		{
+			Random random = new Random();
+
+			array = new int[(int) countElements.Value];
+			array = array.Select( x => random.Next( (int) minLim.Value, (int) ( maxLim.Value + 1 ) ) ).ToArray();
+
+            mergeSort(array, 0, array.Length-1);
+
+
+            /*for ( int i = 0; i < array.Length; i++ )
+			{
+				int min = i;
+				for ( int j = i + 1; j < array.Length; j++ )
+					if ( array[ j ] < array[ min ] )
+						min = j;
+
+				if ( min != i )
+					swap( ref array[ i ], ref array[ min ] );
+
+				drawSort( array );
+				drawMarking();
+				Thread.Sleep( 100 );
+			}*/
+        }
+
+		/*private void swap< T >( ref T a, ref T b )
+		{
+			T c = a;
+			a = b;
+			b = c;
+		}*/
+
+		private void drawSort( int[] array )
+		{
+			bool flag = true;
+			Pen pen = new Pen( Color.DarkOrange );
+
+
+			Graphics graphics = pictureBox1.CreateGraphics();
+			graphics.Clear( Color.Black );
+			for ( int i = (int)minLim.Value; i <= maxLim.Value; i++ )
+			{
+				for ( int j = 0; j < array.Length; j++ )
+				{
+					if ( flag )
+						pen = new Pen( Color.CornflowerBlue );
+					else
+						pen = new Pen( Color.DarkOrange );
+					flag = !flag;
+
+					if ( array[ j ] >= i )
+						graphics.FillRectangle( new SolidBrush( pen.Color ), 15 * j, pictureBox1.Height - 15 * i, 15, 15 );
+				}
+			}
+		}
+
+		private void drawMarking()
+		{
+			Graphics graphics = pictureBox1.CreateGraphics();
+
+			Pen pen = new Pen( Color.DarkGreen );
+			for ( int i = 0; i < pictureBox1.Height; i += 15 )
+				graphics.DrawLine( pen, 0, pictureBox1.Height - i, pictureBox1.Width, pictureBox1.Height - i );
+			for ( int i = 0; i < pictureBox1.Width; i += 15 )
+				graphics.DrawLine( pen, i, 0, i, pictureBox1.Width );
+		}
+
+		private void pictureBox1_Paint( object sender, PaintEventArgs e )
+		{
+			((PictureBox)sender).CreateGraphics().Clear( Color.Black );
+			drawMarking();
+		}
+
+        
+    }
+}
